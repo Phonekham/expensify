865b85d97a609f7d99333e7a6da5703bca89df82
Phonekham <phonekham.dev@gmail.com>
2019-06-06 11:57:58 +0700

    Redux: Subscribing and Dynamic Actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,32 +3,45 @@ import { createStore } from "redux";
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
+      const incrementBy =
+        typeof action.incrementBy === "number" ? action.incrementBy : 1;
       return {
-        count: state.count + 1
+        count: state.count + incrementBy
       };
     case "DECREMENT":
+      const decrementBy =
+        typeof action.decrementBy === "number" ? action.decrementBy : 1;
       return {
-        count: state.count - 1
+        count: state.count - decrementBy
       };
     case "RESET":
       return {
         count: 0
+      };
+    case "SET":
+      return {
+        count: action.count
       };
     default:
       return state;
   }
 });
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch({
+  type: "INCREMENT",
+  incrementBy: 5
+});
 
 store.dispatch({
   type: "INCREMENT"
 });
 store.dispatch({
-  type: "INCREMENT"
-});
-store.dispatch({
-  type: "DECREMENT"
+  type: "DECREMENT",
+  decrementBy: 10
 });
 
 // reset count to 0
@@ -36,4 +49,7 @@ store.dispatch({
   type: "RESET"
 });
 
-console.log(store.getState());
+store.dispatch({
+  type: "SET",
+  count: 101
+});
