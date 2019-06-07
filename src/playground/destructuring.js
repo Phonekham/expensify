0ddccf2cba4348ae67a70930152588639818f35c
Phonekham <phonekham.dev@gmail.com>
2019-06-07 09:13:30 +0700

    array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,15 +1,21 @@
-const person = {
-  name: "Phone",
-  age: 24,
-  location: {
-    city: "VT",
-    temp: 32
-  }
-};
-const { name, age } = person;
-console.log(`${name} is ${age}.`);
+// Object destucturing
+// const person = {
+//   name: "Phone",
+//   age: 24,
+//   location: {
+//     city: "VT",
+//     temp: 32
+//   }
+// };
+// const { name, age } = person;
+// console.log(`${name} is ${age}.`);
 
-const { city, temp: temperature } = person.location;
-if (city && temperature) {
-  console.log(`It's ${temperature} in ${city}`);
-}
+// const { city, temp: temperature } = person.location;
+// if (city && temperature) {
+//   console.log(`It's ${temperature} in ${city}`);
+// }
+
+// Array destructuring
+const address = ["123 street", "VT", "Laos"];
+const [street, city, country] = address;
+console.log(`You are in ${street} ${city}`);
